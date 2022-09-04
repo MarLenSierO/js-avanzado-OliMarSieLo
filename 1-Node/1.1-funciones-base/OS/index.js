482c983eab025b8cc502d6277d0afddc18c2274a
Olivia Sierra <mergarita.leon@gmail.com>
2022-09-04 09:20:03 -0500

    feat: se agrega carpeta Node

---
+++ 1-Node/1.1-funciones-base/OS/index.js
@@ -0,0 +1,13 @@
+const os = require ('os');
+
+console.log ('Versión detallada del sistema: ', os.version());
+console.log('Nombre de la versión del sistema: ', os.platform());
+console.log('Numero de la versión del sistema:' + os.release());
+
+console.log('arquitectura: ',os.arch()); 
+console.log('da información acerca de los cpus: ', os.cpus());
+console.log('Número de procesadores lógicos:' + os.cpus().length);
+console.log('memoria libre en bytes', os.freemem()); 
+console.log('carpeta del usuario actual: ', os.homedir()); 
+console.log('mi direccion ip o mi hostname (local): ', os.hostname()); 
+
---
+++ 1-Node/1.1-funciones-base/global/index.js
@@ -0,0 +1,4 @@
+console.log(this) // {} ó  module.export
+console.log ('Ruta donde está mi archivo: ', __dirname);
+console.log ('Ruta + nombre de mi archivo: ', __filename);
+// funciones que nos permiten acceder al archivo por el simple hecho de tener node.
